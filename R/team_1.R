c7d45aa560c22bbb3a48872f30d096ab82fc835a
GaniAgadil <agadilov@iastate.edu>
2019-03-16 17:16:57 -0500

    function form

--- R/team_1.R
+++ R/team_1.R
@@ -3,17 +3,19 @@ library(tidyverse)
 library(purrr)
 library(sf)
 
-ozbig <- team_1("data/gadm36_AUS_shp/gadm36_AUS_1.shp", 0.1)
-team_1 <- function(file, tolerance) {
-  ozbig <- read_sf("file")
-  oz_st <- maptools::thinnedSpatialPoly(as(ozbig, "Spatial"), tolerance = 0.1, minarea = 0.001, topologyPreserve = TRUE)
+team_1 <- function(file,tolerance) {
+  ozbig <- read_sf(file)
+  oz_st <- maptools::thinnedSpatialPoly(as(ozbig, "Spatial"), tolerance = tolerance, minarea = 0.001, topologyPreserve = TRUE)
   oz <- st_as_sf(oz_st)
   df.oz.purr <- oz$geometry %>%
-    map_depth(3, data.frame) %>%
-    purrr::flatten() %>%
-    purrr::flatten() %>%
-    bind_rows(.id = "group") %>%
-    rename("lat" = y, "long" = x)
+      map_depth(3, data.frame) %>%
+      purrr::flatten() %>%
+      purrr::flatten() %>%
+      bind_rows(.id = "group") %>%
+      rename("lat" = y, "long" = x)
   return(df.oz.purr)
 }
 
+# The retun value is the dataframe
+
+result <- team_1(file="data/gadm36_AUS_shp/gadm36_AUS_1.shp", tolerance=0.1)
