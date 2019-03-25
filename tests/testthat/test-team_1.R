8a5f67ff9732a4417c2a04819f41d81e88e44355
GaniAgadil <agadilov@iastate.edu>
2019-03-25 18:21:19 -0500

    ok

--- tests/testthat/test-team_1.R
+++ tests/testthat/test-team_1.R
@@ -2,7 +2,7 @@ context("test-team_1.R")
 
 test_that("team_1 fucnction provides a dataframe", {
     filepath <- system.file("gadm36_AUS_shp/gadm36_AUS_1.shp", package ="stat585lab3package")
-    expect_s3_class(team_10(filepath, 0.01), "data.frame")
+    expect_s3_class(team_1(filepath, 0.01), "data.frame")
     })
 
 test_that("team_1 fucnction has correct names of the columns", {
