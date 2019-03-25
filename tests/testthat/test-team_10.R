9b4eb12be46baff077e553c366e9fce5714318a8
kgulzina <gkuttubekova@gmail.com>
2019-03-25 17:55:19 -0500

    Updated test file for team_10.R

--- tests/testthat/test-team_10.R
+++ tests/testthat/test-team_10.R
@@ -1,4 +1,5 @@
 context("test-team_10")
+filepath #<- system.file("data/gadm36_AUS_shp/gadm36_AUS_1.shp", ".shp", package = ???)
 
 test_that("takes in correct path", {
     # expect an error due to incorrect path
@@ -12,10 +13,13 @@ test_that("takes in correct path", {
 test_that("tolerance is correct", {
     # expect error due to tolerance out of range
     expect_error(team_10(file = "./data/gadm36_AUS_shp/gadm36_AUS_1.shp", 13))
+    # expect error due to not numeric tolerance
+    expect_error(team_10(file = "./data/gadm36_AUS_shp/gadm36_AUS_1.shp", "test"))
 })
 
 
 test_that("output is dataframe", {
     # expect data frame
-    expect_s3_class(team_10("./data/gadm36_AUS_shp/gadm36_AUS_1.shp", 0.01), "data.frame")
+    filepath <- system.file("gadm36_AUS_shp/gadm36_AUS_1.shp", package ="stat585lab3package")
+    expect_s3_class(team_10(filepath, 0.01), "data.frame")
 })
