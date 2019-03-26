183cf1f687e419f96ab374f543c0ce8fd32c295d
Gani Agadilov <39811941+GaniAgadil@users.noreply.github.com>
2019-03-26 00:40:18 -0500

    Do not change this file!

---
+++ R/team_1.R
@@ -0,0 +1,27 @@
+#' The function that allowed to create a data frame from a geometry variable
+#'
+#'
+#'
+#'@param file file path is a file path to a shape file
+#'@param tolerance is value used for thinning the polygon
+#'@return df.oz.purr
+#'@import tidyverse
+#'@import purrr
+#'@import sf
+#'@export
+
+
+
+team_1 <- function(file,tolerance) {
+  ozbig <- read_sf(file)
+  oz_st <- maptools::thinnedSpatialPoly(as(ozbig, "Spatial"), tolerance = tolerance, minarea = 0.001, topologyPreserve = TRUE)
+  oz <- st_as_sf(oz_st)
+  df.oz.purr <- oz$geometry %>%
+      map_depth(3, data.frame) %>%
+      purrr::flatten() %>%
+      purrr::flatten() %>%
+      bind_rows(.id = "group") %>%
+      rename("lat" = y, "long" = x)
+  return(df.oz.purr)
+}
+
