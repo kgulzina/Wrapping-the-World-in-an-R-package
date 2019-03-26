ab64631c059535a59289618cd372933e482e9ae9
Jing <46006633+jingzhao19@users.noreply.github.com>
2019-03-25 23:50:59 -0500

    Add files via upload

---
+++ tests/testthat/test-team_11.R
@@ -0,0 +1,8 @@
+context("test-team_11")
+
+test_that("output is dataframe", {
+    # expect data frame output
+     filepath <- system.file("extdata/gadm36_AUS_1.shp", package ="stat585lab3package")
+    expect_s3_class(team_11(filepath, 0.01), "data.frame")
+})
+
