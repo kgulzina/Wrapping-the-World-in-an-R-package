9e515903a94355a535d0f9a2ec68f7ef6d3dc660
Jing <46006633+jingzhao19@users.noreply.github.com>
2019-03-26 17:58:46 -0500

    Update test-team_11.R

--- tests/testthat/test-team_11.R
+++ tests/testthat/test-team_11.R
@@ -2,7 +2,7 @@ context("test-team_11")
 
 test_that("output is dataframe", {
     # expect data frame output
-     filepath <- system.file("extdata/gadm36_AUS_1.shp", package ="stat585lab3package")
+     filepath <- system.file("gadm36_AUS.shp/gadm36_AUS_1.shp", package ="stat585lab3package")
     expect_s3_class(team_11(filepath, 0.01), "data.frame")
 })
 
